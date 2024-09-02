b1997af4f1585931481b2f0a3f14f4d025c397d6
FlorianRauchberger <61333943+FlorianRauchberger@users.noreply.github.com>
2024-09-02 08:14:23 +0200

    Created the docs

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>BluePrint</span>,
+  project: {
+    link: 'https://github.com/FlorianRauchberger/blueprint',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/FlorianRauchberger/blueprint',
+  footer: {
+    text: 'Blueprint',
+  },
+}
+
+export default config
